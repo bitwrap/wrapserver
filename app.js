94e331b2d7b6d0b9c22eb304cfa02dafbe67144f
stackdump <myork@stackdump.com>
2017-01-24 23:38:39 -0800

    WIP

--- app.js
+++ app.js
@@ -1,53 +1,63 @@
 var express = require('express');
 var app = express();
 var jsdom = require('jsdom');
+var request = require('request');
+
 var fs = require('fs');
 var snap_src = fs.readFileSync('./node_modules/snapsvg/dist/snap.svg-min.js', 'utf-8');
 
 var templates = {
-  bitwrap: require('./lib/bitwrap.js'),
+  machine: require('./lib/machine.js'),
   counter: require('./lib/counter.js')
 }
 
 function render(paper, req) {
-
   var tpl = templates[req.params.template];
 
   if (! tpl) {
     return false
   }
 
-  console.log({
-    query: req.query,
-    params: req.params
-  });
-
-  return tpl.render(
+  return tpl(
     paper,
-    { query: req.query, params: req.params }
+    { query: req.query, params: req.params, body: req.body }
   );
+}
+
+function abort(res) {
+  res.writeHead(404, {'Content-Type': 'image/svg+xml' });
+  res.end('', 'utf-8');
 }
 
 app.get('/:template/:oid.svg', function (req, res) {
 
-  jsdom.env( '', // empty body
-    [], // no external scripts 
-    { src: [ snap_src ] },
-    function (err, window) {
-      var paper = new window.Snap();
-      var _tpl = req.params.template;
+  var uri = 'http://127.0.0.1:8080/' + req.params.template + '/' + req.params.oid + '.json';
 
-      if (false !== render(paper, req)) {
-        res.writeHead(200, {'Content-Type': 'image/svg+xml' });
-        res.end(paper.toString(), 'utf-8');
-      } else {
-        res.writeHead(404, {'Content-Type': 'image/svg+xml' });
-        res.end('', 'utf-8');
-      }
+  request(uri, function (error, response, body) {
+    if (error || response.statusCode != 200) {
+      abort(res);
+    } else {
+      data = JSON.parse(body);
 
-      window.close();
+      jsdom.env( '', // empty body
+        [], // no external scripts 
+        { src: [ snap_src ] },
+        function (err, window) {
+          var paper = new window.Snap();
+          req['body'] = data;
+
+          if (false !== render(paper, req)) {
+            res.writeHead(200, {'Content-Type': 'image/svg+xml' });
+            res.end(paper.toString(), 'utf-8');
+          } else {
+            abort(res);
+          }
+
+          window.close();
+      });
     }
-  );
+  });
+
 })
 
 app.listen(8000, function () {
