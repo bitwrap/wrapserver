9343ff4ce54e3e9f052309fb19c24c3c632f90c2
stackdump <myork@stackdump.com>
2017-08-05 10:40:53 -0500

    WIP

--- templates/editor.js
+++ templates/editor.js
@@ -1,48 +1,19 @@
 var size = 20;
 
-function draw_transition(paper, o) {
-  // FIXME: alter to draw a 'transition' PNML element
-
+function draw_icon(paper, o) {
   paper
-    .rect({
-      'x': o.x,
-      'y': o.y,
-      'width': o.width,
-      'height': o.height,
-      'rx': o.rx,
-      'ry': o.ry,
-    })
-    .attr({
-      'id': o.id,
-      'fill': '#eee',
-      'stroke': '#ccc',
-      'strokeWidth': 2,
-      'class': 'BG'
-    });
-
+    .rect({'x': o.x, 'y': o.y, 'width': o.width, 'height': o.height, 'rx': o.rx, 'ry': o.ry})
+    .attr({'id': o.id, 'fill': '000', 'fill-opacity': 0.01, 'stroke': '#ccc', 'strokeWidth': 2, 'class': 'icon'});
 }
 
 function draw_arc(source, target) {
   //FIXME
 }
 
-function draw_place(paper, coords, seq) {
-  // draw PNML 'place' element
-  var x = coords[1] * size * 2 + size;
-  var y = coords[0] * size * 2 + size;
-
+function draw_place(paper, o) {
   paper
-    .circle({
-      'cx': x,
-      'cy': y,
-      'r': (size/2)
-    }).attr({
-        'id': 'event-' + seq,
-        'class': 'O',
-        'fill-opacity': '0',
-        'stroke': '#000',
-        'strokeWidth': 5
-    });
+    .circle({'cx': o.x, 'cy': o.y, 'r': (size * 4/5)})
+    .attr({'id': o.id, 'class': 'place', 'fill-opacity': '0', 'stroke': '#000', 'strokeWidth': 2});
 }
 
 function draw_grid_point(paper, o) {
@@ -50,46 +21,45 @@ function draw_grid_point(paper, o) {
   // TODO add selector point for 'snapping' PNML element placement
 }
 
-function draw_grid(paper, o) {
-  var factor = size * 2
 
-  seq = Array.apply(null, Array(32)).map(function (_, i) {return i;});
+function draw_menu(paper, o) {
+  var factor = size * 2;
+  var y0 = 5;
+  var x0 = 0;
 
-  for ( y0 in seq ) {
-    for ( x0 in seq) {
+  var group = paper.group().attr({ 'id': 'edt-menu'});
 
-      draw_grid_point(paper, {
-        "id": 'square-' + y0 + x0,
-        "x": x0 * factor,
-        "y": y0 * factor,
-        "width": factor,
-        "height": factor,
-        "rx": 0,
-        "ry": 0
-      });
+  tool_pallet = {
+    'select': function (){
+    },
+
+    'place': function () {
+      draw_place(group, { "id": 'icon-' + label, "x": x0 * factor + size, "y": y0 + size, "width": factor, "height": factor, "rx": 0, "ry": 0 })
+    },
+
+    'transition': function () {
+    },
+
+    'arc': function () {
+    },
+
+    'token': function () {
     }
+  }
+
+  for ( label in tool_pallet) {
+    tool_pallet[label]();
+    draw_icon(group, {"id": label, "x": x0 * factor, "y": y0, "width": factor, "height": factor, "rx": 0, "ry": 0});
+    x0 += 1
   }
 }
 
 module.exports = { 
-  // TODO: export functions / DSL for front-end GUI
+  'render' : function (w, callback) {
+    w.paper.attr({'width': '1200', 'height': '800'})
+    draw_menu(w.paper, {})
 
-  'render' : function (widget, callback) {
-    widget.paper.attr({'width': '1200', 'height': '800'})
-
-    console.log(widget.paper)
-    // KLUDGE: May need to request SVG outside of widget template
-    // if we want this to work outside of the browser
-    var image_url  = '/image/editor.svg';
-
-    if (widget.config.wrapserver != undefined ) {
-      image_url = widget.config.wrapserver + image_url;
-    }
-
-    if ( widget.data != undefined && callback != undefined) {
-      callback(widget.data)
-    }
-
+    if ( w.data && callback) { callback(w.data) }
   },
 
   'resource': function (base_uri, params) {
